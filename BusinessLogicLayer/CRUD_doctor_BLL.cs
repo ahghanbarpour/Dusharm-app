0747f6eaac6deeab3ef5f461c3736dbe5bb7abd1
Amirhossein Ghanbarpour <72201011+ahghanbarpour@users.noreply.github.com>
2022-07-01 19:43:13 +0430

    BLL

---
+++ BusinessLogicLayer/CRUD_doctor_BLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using BusinessEntity;
+using BusinessEntity.ViewModels;
+
+namespace BusinessLogicLayer
+{
+    public class CRUD_doctor_BLL
+    {
+        CRUD_doctor_DAL cdd = new CRUD_doctor_DAL();
+        public void create(doctor d)
+        {
+            cdd.create(d);
+        }
+        public List<DoctorViewModel> Read()
+        {
+            return cdd.Read();
+        }
+        //public List<DoctorViewModel> read(string s)
+        //{
+        //    return cdd.read(s);
+        //}
+        public List<DoctorViewModel> read(string s)
+        {
+            return cdd.read(s);
+        }
+        public string update(int id, doctor dn)
+        {
+            return cdd.update(id, dn);
+        }
+    }
+}
---
+++ BusinessLogicLayer/CRUD_patient_BLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntity;
+using BusinessEntity.ViewModels;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class CRUD_patient_BLL
+    {
+        CRUD_patient_DAL cpd = new CRUD_patient_DAL();
+        public string create(patient p)
+        {
+            if (!cpd.res(p))
+            {
+                return cpd.create(p);
+            }
+            else
+            {
+                return "روز و ساعت مورد نظر تداخل دارد";
+            }
+        }
+        public List<PatientViewModel> Read()
+        {
+            return cpd.Read();
+        }
+        public List<PatientViewModel> read(string s)
+        {
+            return cpd.read(s);
+        }
+        public patient read(int id)
+        {
+            return cpd.read(id);
+        }
+        public string update(int id, patient pn)
+        {
+            return cpd.update(id, pn);
+        }
+        public string delete(int id)
+        {
+            return cpd.delete(id);
+        }
+        public bool check(patient p)
+        {
+            return cpd.check(p);
+        }
+
+
+    }
+}
